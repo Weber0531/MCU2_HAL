e39162531a76b277ba60c497ca7724547c9de409
Weber0531 <ooooppppweber@gmail.com>
2025-06-20 18:57:58 +0800

    create a new project

---
+++ Timer_InputCapture_1/Core/Src/it.c
@@ -0,0 +1,26 @@
+/*
+ * it.c
+ *
+ *  Created on: Jun 18, 2025
+ *      Author: weber
+ */
+
+
+#include "main_app.h"
+extern TIM_HandleTypeDef htimer2;
+
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+	HAL_IncTick();
+	HAL_SYSTICK_IRQHandler();
+}
+
+
+void TIM2_IRQHandler(void)
+{
+	HAL_TIM_IRQHandler(&htimer2);
+}
