7b99798ac1ca1e99ca4fe31d13546f11f30f2943
Weber0531 <ooooppppweber@gmail.com>
2025-07-01 18:10:38 +0800

    template is done

--- Timer_OutputCompare_1/Core/Src/msp.c
+++ Timer_OutputCompare_1/Core/Src/msp.c
@@ -25,31 +25,31 @@ void HAL_MspInit(void)
 }
 
 
-//void HAL_UART_MspInit(UART_HandleTypeDef *huart)
-//{
-//	GPIO_InitTypeDef gpio_uart;
-//
-//	// Here we are going to do the low level inits of the USART2 peripheral
-//	// 1. enable the clock for the USART2 peripheral as well as for GPIOA peripheral
-//	__HAL_RCC_USART2_CLK_ENABLE();
-//	__HAL_RCC_GPIOA_CLK_ENABLE();
-//
-//	// 2. Do the pin muxing configurations
-//	gpio_uart.Mode = GPIO_MODE_AF_PP;
-//	gpio_uart.Alternate = GPIO_AF7_USART2;
-//	gpio_uart.Pull = GPIO_PULLUP;
-//	gpio_uart.Speed = GPIO_SPEED_FREQ_LOW;
-//
-//	gpio_uart.Pin = GPIO_PIN_2;
-//	HAL_GPIO_Init(GPIOA, &gpio_uart);
-//
-//	gpio_uart.Pin = GPIO_PIN_3;
-//	HAL_GPIO_Init(GPIOA, &gpio_uart);
-//
-//	// 3. Enable the IRQ and set up the priority (NVIC settings)
-//	HAL_NVIC_EnableIRQ(USART2_IRQn);
-//	HAL_NVIC_SetPriority(USART2_IRQn, 15, 0);
-//}
+void HAL_UART_MspInit(UART_HandleTypeDef *huart)
+{
+	GPIO_InitTypeDef gpio_uart;
+
+	// Here we are going to do the low level inits of the USART2 peripheral
+	// 1. enable the clock for the USART2 peripheral as well as for GPIOA peripheral
+	__HAL_RCC_USART2_CLK_ENABLE();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+
+	// 2. Do the pin muxing configurations
+	gpio_uart.Mode = GPIO_MODE_AF_PP;
+	gpio_uart.Alternate = GPIO_AF7_USART2;
+	gpio_uart.Pull = GPIO_PULLUP;
+	gpio_uart.Speed = GPIO_SPEED_FREQ_LOW;
+
+	gpio_uart.Pin = GPIO_PIN_2;
+	HAL_GPIO_Init(GPIOA, &gpio_uart);
+
+	gpio_uart.Pin = GPIO_PIN_3;
+	HAL_GPIO_Init(GPIOA, &gpio_uart);
+
+	// 3. Enable the IRQ and set up the priority (NVIC settings)
+	HAL_NVIC_EnableIRQ(USART2_IRQn);
+	HAL_NVIC_SetPriority(USART2_IRQn, 15, 0);
+}
 
 
 void HAL_TIM_OC_MspInit(TIM_HandleTypeDef *htim)
--- Timer_OutputCompare_PWM_1/Core/Src/main_app.c
+++ Timer_OutputCompare_PWM_1/Core/Src/main_app.c
@@ -6,11 +6,163 @@
  */
 
 
+/* Includes ------------------------------------------------------------------*/
 #include "main_app.h"
+
+/* Private function prototypes -----------------------------------------------*/
+void GPIO_Init(void);
+void Error_handler(void);
+void TIMER2_Init(void);
+void UART2_Init(void);
+void SystemClock_Config_HSE(uint8_t clock_freq);
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htimer2;
+UART_HandleTypeDef huart2;
 
 int main(void)
 {
+	HAL_Init();
+
+	SystemClock_Config_HSE(SYS_CLOCK_FREQ_50_MHZ);
+
+	GPIO_Init();
+
+	TIMER2_Init();
+
+	UART2_Init();
+
 	while(1);
 
 	return 0;
+}
+
+
+void SystemClock_Config_HSE(uint8_t clock_freq)
+{
+	RCC_OscInitTypeDef osc_init;
+	RCC_ClkInitTypeDef clk_init;
+	uint32_t FLatency = 0;
+
+	osc_init.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+	osc_init.HSEState = RCC_HSE_ON;
+	osc_init.PLL.PLLState = RCC_PLL_ON;
+	osc_init.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+
+	switch(clock_freq)
+	{
+	case SYS_CLOCK_FREQ_50_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 50;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 2;
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+							 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+
+		FLatency = FLASH_ACR_LATENCY_1WS;
+		break;
+	}
+	case SYS_CLOCK_FREQ_84_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 84;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 2;
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+							 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV2;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV1;
+
+		FLatency = FLASH_ACR_LATENCY_2WS;
+		break;
+	}
+	case SYS_CLOCK_FREQ_120_MHZ:
+	{
+		osc_init.PLL.PLLM = 4;
+		osc_init.PLL.PLLN = 120;
+		osc_init.PLL.PLLP = RCC_PLLP_DIV2;
+		osc_init.PLL.PLLQ = 2;
+
+		clk_init.ClockType = RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | \
+							 RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+		clk_init.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+		clk_init.AHBCLKDivider = RCC_SYSCLK_DIV1;
+		clk_init.APB1CLKDivider = RCC_HCLK_DIV4;
+		clk_init.APB2CLKDivider = RCC_HCLK_DIV2;
+
+		FLatency = FLASH_ACR_LATENCY_3WS;
+		break;
+	}
+	default:
+		return;
+	}
+
+	if(HAL_RCC_OscConfig(&osc_init) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	if(HAL_RCC_ClockConfig(&clk_init, FLatency) != HAL_OK)
+	{
+		Error_Handler();
+	}
+
+	/* Configure the systick timer interrupt frequency (for every 1 ms) */
+	uint32_t hclk_freq = HAL_RCC_GetHCLKFreq();
+	HAL_SYSTICK_Config(hclk_freq / 1000);
+
+	/* Configure the Systick */
+	HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+	/* SysTick_IRQn interrupt configuration */
+	HAL_NVIC_GetPriority(SysTick_IRQn, 0, 0);
+}
+
+
+void GPIO_Init(void)
+{
+	__HAL_RCC_GPIOD_CLK_ENABLE();
+	GPIO_InitTypeDef gpioled;
+	gpioled.Pin = GPIO_PIN_12;
+	gpioled.Mode = GPIO_MODE_OUTPUT_PP;
+	gpioled.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOD, &gpioled);
+}
+
+
+void TIMER2_Init(void)
+{
+
+}
+
+
+void UART2_Init(void)
+{
+	huart2.Instance = USART2;
+	huart2.Init.BaudRate = 115200;
+	huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart2.Init.Mode = UART_MODE_TX_RX;
+	huart2.Init.Parity = UART_PARITY_NONE;
+	huart2.Init.StopBits = UART_STOPBITS_1;
+	huart2.Init.WordLength = UART_WORDLENGTH_8B;
+
+	if(HAL_UART_Init(&huart2) != HAL_OK)
+	{
+		Error_Handler();
+	}
+}
+
+
+void Error_handler(void)
+{
+	while(1);
 }
